4392fb3077a6b83c64bb3bcf8a9512862ecb9a25
snowdev1205 <snowdev1205@gmail.com>
2022-04-27 22:49:15 +0200

    new

---
+++ hardhat.config.js
@@ -0,0 +1,43 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  //defaultNetwork: "hardhat",
+
+
+  solidity: "0.8.4",
+
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+    /*
+    ropsten: {
+      // url: "https://ropsten.infura.io/v3/your-project-id", 
+      // accounts: [`0x${your-private-key}`] 
+      url: "https://ropsten.infura.io/v3/ba3bbe8d368c4efbad82c345a4c5bbd4",
+      accounts: [`0x${ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80}`]
+    }
+    */
+  }
+
+  
+};
